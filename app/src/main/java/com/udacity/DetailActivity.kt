273f914ac70e2aecd2b089ffcb4c581c72089275
Ricardo Ducci Popi <ducci.popi@gmail.com>
2021-08-27 02:08:17 -0300

    Details and MotionLayout

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,8 +1,11 @@
 package com.udacity
 
+import android.app.DownloadManager
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,6 +13,46 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        // Dismiss the notification
+        getSystemService(NotificationManager::class.java).cancelAll()
+
+        // Get information from the notification intent
+        var filename = R.string.no_filename_defined
+        var downloadId = -1L
+        intent.extras?.let {
+            filename = it.getInt(EXTRA_FILE_NAME, R.string.no_filename_defined)
+            downloadId = it.getLong(EXTRA_DOWNLOAD_ID)
+        }
+
+        // Check download status
+        val downloadManager = getSystemService(DownloadManager::class.java) as DownloadManager
+        val request = downloadManager.query(DownloadManager.Query().setFilterById(downloadId))
+        var success = false
+        if (request.moveToFirst())
+            success =
+                request.getInt(request.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL
+
+        // Set text and color based in download status
+        filename_text.text = getString(filename)
+        status_text.text =
+            getString(if (success) R.string.download_success else R.string.download_failed)
+
+        val color = getColor(if (success) R.color.colorSuccess else R.color.colorFailed)
+        cloud_status.setColorFilter(color)
+        status_text.setTextColor(color)
+
+        close_button.setOnClickListener {
+            finish()
+        }
+
+
+    }
+
+
+    companion object {
+        const val EXTRA_FILE_NAME = "file_name"
+        const val EXTRA_DOWNLOAD_ID = "download_id"
     }
 
 }
--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.udacity
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
@@ -8,7 +9,9 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,11 +20,10 @@ import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var downloadID: Long = 0
-
     private lateinit var notificationManager: NotificationManager
-    private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+
+    private val downloadMap = mutableMapOf<Long, Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,20 +32,79 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        setupNotificationChannel()
+
         custom_button.setOnClickListener {
-            //download()
+            val option = options_group.checkedRadioButtonId
+            if (option == -1) {
+                Toast.makeText(this, R.string.no_option_selected_toast_message, Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                val optionInfos = URLS[option]
+                optionInfos?.let {
+                    val downloadId = download(getString(it.first))
+                    downloadMap.put(downloadId, it.second)
+                }
+            }
+        }
+    }
+
+    private fun setupNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                getString(CHANNEL_NAME),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            notificationChannel.enableLights(true)
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = getString(CHANNEL_DESCRIPTION)
+
+            notificationManager = this.getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
         }
     }
 
     private val receiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+        override fun onReceive(context: Context, intent: Intent) {
+            val id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            val downloadInfo = downloadMap.remove(id)
+
+            downloadInfo?.let {
+                val contentIntent = Intent(applicationContext, DetailActivity::class.java)
+                contentIntent.putExtra(DetailActivity.EXTRA_FILE_NAME, downloadInfo)
+                contentIntent.putExtra(DetailActivity.EXTRA_DOWNLOAD_ID, id)
+
+                val pendingIntent = PendingIntent.getActivity(
+                    applicationContext,
+                    NOTIFICATION_ID,
+                    contentIntent,
+                    PendingIntent.FLAG_CANCEL_CURRENT
+                )
+
+                val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+                    .setContentTitle(getString(R.string.notification_title))
+                    .setContentText(getString(R.string.notification_description))
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .addAction(
+                        android.R.drawable.ic_dialog_info,
+                        getString(R.string.notification_button),
+                        pendingIntent
+                    )
+
+                notificationManager.notify(NOTIFICATION_ID, builder.build())
+            }
         }
     }
 
-    private fun download() {
+    private fun download(url: String): Long {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -51,14 +112,21 @@ class MainActivity : AppCompatActivity() {
                 .setAllowedOverRoaming(true)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        return downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
     companion object {
-        private const val URL =
-            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
-        private const val CHANNEL_ID = "channelId"
+        private val URLS = mapOf(
+            R.id.option_glide to (R.string.glide_url to R.string.glide_info),
+            R.id.option_loadapp to (R.string.loadapp_url to R.string.loadapp_info),
+            R.id.option_retrofit to (R.string.retrofit_url to R.string.retrofit_info)
+        )
+
+        private const val NOTIFICATION_ID = 0xCAFE
+
+        private const val CHANNEL_ID = "download_notifications"
+        private const val CHANNEL_NAME = R.string.channel_name
+        private const val CHANNEL_DESCRIPTION = R.string.channel_description
     }
 
 }
